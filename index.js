e14baf42853c5ffd35d687eacba7f4f25078e902
martin835 <64438132+martin835@users.noreply.github.com>
2022-01-10 08:36:28 +0100

    5 minutes count-down added

--- index.js
+++ index.js
@@ -347,6 +347,42 @@ for (i = 0; i < 4; i++) {
 
     // Create a countdown that displays the remaining time of an offer in the page (starting from 5 minutes),
     // to give a sense of urgency to the user to book an appartment before the offer ends.
+
+   
+   function getTimeRemaining(endtime) {
+     const total = Date.parse(endtime) - Date.parse(new Date());
+     const seconds = Math.floor((total / 1000) % 60);
+     const minutes = Math.floor((total / 1000 / 60) % 60);
+     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+     const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+     return {
+       total,
+       days,
+       hours,
+       minutes,
+       seconds,
+     };
+   }
+   
+   
+   
+    function initializeClock(id, endtime) {
+      const clock = document.getElementById(id);
+      const timeinterval = setInterval(() => {
+        const t = getTimeRemaining(endtime);
+        clock.innerHTML = t.minutes +":" + t.seconds;
+        if (t.total <= 0) {
+          clearInterval(timeinterval);
+        }
+      }, 1000);
+    }
+
+    const timeInMinutes = 5;
+    const currentTime = Date.parse(new Date());
+    const deadline = new Date(currentTime + timeInMinutes * 60 * 1000);
+
+    initializeClock("clockdiv", deadline);
     
     // The countdown should appear in a modal after 1 minute from the page load.
     // Display an alert when the time is up.
